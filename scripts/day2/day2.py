f92ff9ac1d2f57c0e16ba8a03c5e76b7ae0219d3
Landcruiser87 <andyheroy@gmail.com>
2024-12-02 14:41:23 -0600

    code cleanup

--- scripts/day2/day2.py
+++ scripts/day2/day2.py
@@ -94,12 +94,12 @@ def main():
 
     #Solve part A
     resultA = part_A()
-    logger.info(f"part A solution: \n{resultA}\n")
+    # logger.info(f"part A solution: \n{resultA}\n")
     # support.submit_answer(DAY, YEAR, 1, resultA)
 
     #Solve part B
     resultB = part_B()
-    logger.info(f"part B solution: \n{resultB}\n")
+    # logger.info(f"part B solution: \n{resultB}\n")
     # support.submit_answer(DAY, YEAR, 2, resultB)
 
     #Recurse lines of code
